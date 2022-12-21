179fc203166f528a28fddd6a649204659b0f6d26
GABRIEL VIRGINO DE OLIVEIRA <99374967+Gabriel9816@users.noreply.github.com>
2022-12-21 03:20:41 -0400

    edit

--- main.py
+++ main.py
@@ -8,26 +8,12 @@ import os
 import random
 import requests
 
-
 end_ip = "http://192.168.0.191:5000"
-user1 = None
-user2 = None
-
-
 class Manager(ScreenManager):
     pass
 
-
 class Home(Screen):
     pass
-
-
-""" class Chat(Screen):
-  pass
-
-class CameraClick(Screen):
-  pass """
-
 
 class MyApp(App):
     def build(self):
@@ -35,7 +21,7 @@ class MyApp(App):
         return self.file
 
     def new_conversation(self):
-        request = requests.get(end_ip+"/new_conversation")
+        request=requests.get(end_ip+"/new_conversation")
         json = request.json()
         room_key = json['path']
         self.file.get_screen(
@@ -43,69 +29,37 @@ class MyApp(App):
         return
 
     def join_room(self):
-        cod_room = {"data": self.file.get_screen(
-            'home').ids["inpt_codigo_room"].text}
+        cod_room = {"data": self.file.get_screen('home').ids["inpt_codigo_room"].text}
         print(cod_room)
-        response = requests.post(end_ip+"/join_room", json=cod_room)
+        response = requests.post(end_ip+"/join_room",json=cod_room)
         message = response.json()
         if message['message'] == True:
             print(message['message'])
         if message['message'] == False:
-            self.file.get_screen(
-                'home').ids['idsala'].text = f'codigo da sala inexistente'
+            self.file.get_screen('home').ids['idsala'].text = f'codigo da sala inexistente'
 
     def capture(self):
         cod_room = self.file.get_screen('home').ids["inpt_codigo_room"].text
-        user = "user2"
-        print(cod_room)
-        if user == "user1":
-            camera = self.file.get_screen('camera').ids['camera']
-            camera.export_to_png("user1.png")
-            files = {
-                "file": "/home/gabrielpclinux/Downloads/chat/static/upload/user1.png",
-                "code_room": cod_room,
-                "user": user
-            }
-        if user == "user2":
-            camera = self.file.get_screen('camera').ids['camera']
-            camera.export_to_png("user2.png")
-            files = {
-                "file": "/home/gabrielpclinux/Downloads/chat/static/upload/user2.png",
-                "code_room": cod_room,
-                "user": user
-            }
-        print(files)
-        requests.post(end_ip+"/new_message", json=files)
-
-    def capture(self):
-        cod_room = self.file.get_screen('home').ids["inpt_codigo_room"].text
-        user = "user2"
+        user = "user1"
         print(cod_room)
         if user == "user1":
             camera = self.file.get_screen('home').ids['camera']
             camera.export_to_png("user1.png")
-            files = {
-                "file": "/home/gabrielpclinux/Downloads/chat/user1.png",
-                "code_room": cod_room,
-                "user": user
-            }
+            data = {"code_room":cod_room,"user":user}
+            file = {'file':open('user1.png','rb')}
+            requests.post(end_ip+"/new_message",files=file,data=data)
         if user == "user2":
-            camera = self.file.get_screen('home').ids['camera']
+            camera = self.file.get_screen('camera').ids['camera']
             camera.export_to_png("user2.png")
-            files = {
-                "file": "/home/gabrielpclinux/Downloads/chat/user2.png",
-                "code_room": cod_room,
-                "user": user
-            }
-        print(files)
-        requests.post(end_ip+"/new_message", json=files)
-
+            data = {"code_room":cod_room,"user":user}
+            file = {'file':open('user2.png','rb')}
+            requests.post(end_ip+"/new_message",files=file,data=data)
+            
     def get_message(self):
-        request = requests.get(end_ip+"/get_message")
+        cod_room = {"cd_room":self.file.get_screen('home').ids["inpt_codigo_room"].text}
+        request=requests.post(end_ip+"/get_message",json=cod_room)
         json = request.json()
         room_key = json['path']
-        print(room_key)
-        self.file.get_screen('home').ids['imguser'].source = room_key
-
-
+        self.file.get_screen('home').ids['imguser'].source = room_key 
+        
 MyApp().run()
